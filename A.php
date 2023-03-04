e6ac600e1c543e08c81ac9cabd1588d3d6b0d64d
P21133 <99970058+P21133@users.noreply.github.com>
2023-03-04 23:05:28 +0200

    Add files via upload

---
+++ A.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$phone= $_POST['phone']
+$to = "dimitraamanta@gmail.com";
+$subject = "Mail From website";
+$txt ="name = ". $name . "\r\n  email = " . $email . "\r\n message =" . $message.
+"\r\n phone =". $phone;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
